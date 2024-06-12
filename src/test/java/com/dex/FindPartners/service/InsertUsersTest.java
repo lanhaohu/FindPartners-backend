380de8794fc2a85718bfcc5cc9cb55da862200f1
lanhaohu <lanhaohu11@gmail.com>
2024-06-12 09:00:02 +0800

    blog first commit

---
+++ src/test/java/com/dex/FindPartners/service/InsertUsersTest.java
@@ -0,0 +1,100 @@
+package com.dex.FindPartners.service;
+
+import com.yupi.yupao.model.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+/**
+ * 导入用户测试
+ *
+
+ */
+@SpringBootTest
+public class InsertUsersTest {
+
+    @Resource
+    private UserService userService;
+
+    private ExecutorService executorService = new ThreadPoolExecutor(40, 1000, 10000, TimeUnit.MINUTES, new ArrayBlockingQueue<>(10000));
+
+    /**
+     * 批量插入用户
+     */
+    @Test
+    public void doInsertUsers() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        final int INSERT_NUM = 1000;
+        List<User> userList = new ArrayList<>();
+        for (int i = 0; i < INSERT_NUM; i++) {
+            User user = new User();
+            user.setUsername("假蓝浩虎");
+            user.setUserAccount("fakelanlaohu");
+            user.setAvatarUrl("https://th.bing.com/th/id/R.e6779e3caaed70b70ed98bd326587fc3?rik=nrxIUZbIx6pUuQ&riu=http%3a%2f%2fimg.mp.itc.cn%2fupload%2f20170521%2f14d8f54c88bd44a0a5ec80302647fade_th.jpg&ehk=P%2ftLdmcWh8DpmSKkAsllBgcAosQ%2bZ0SqemQMtYd3V50%3d&risl=&pid=ImgRaw&r=0");
+            user.setGender(0);
+            user.setUserPassword("12345678");
+            user.setPhone("123");
+            user.setEmail("123@qq.com");
+            user.setTags("[]");
+            user.setUserStatus(0);
+            user.setUserRole(0);
+            user.setPlanetCode("11111111");
+            userList.add(user);
+        }
+        // 20 秒 10 万条
+        userService.saveBatch(userList, 100);
+        stopWatch.stop();
+        System.out.println(stopWatch.getTotalTimeMillis());
+    }
+
+    /**
+     * 并发批量插入用户
+     */
+    @Test
+    public void doConcurrencyInsertUsers() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        // 分十组
+        int batchSize = 5000;
+        int j = 0;
+        List<CompletableFuture<Void>> futureList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            List<User> userList = new ArrayList<>();
+            while (true) {
+                j++;
+                User user = new User();
+                user.setUsername("假蓝浩虎");
+                user.setUserAccount("fakelanhaohu");
+                user.setAvatarUrl("https://pic3.zhimg.com/50/v2-8aabe98b3dd897574d94314706b63d7a_hd.jpg?source=1940ef5c");
+                user.setGender(0);
+                user.setUserPassword("12345678");
+                user.setPhone("123");
+                user.setEmail("123@qq.com");
+                user.setTags("[]");
+                user.setUserStatus(0);
+                user.setUserRole(0);
+                user.setPlanetCode("11111111");
+                userList.add(user);
+                if (j % batchSize == 0) {
+                    break;
+                }
+            }
+            // 异步执行
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+                System.out.println("threadName: " + Thread.currentThread().getName());
+                userService.saveBatch(userList, batchSize);
+            }, executorService);
+            futureList.add(future);
+        }
+        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[]{})).join();
+        // 20 秒 10 万条。
+        stopWatch.stop();
+        System.out.println(stopWatch.getTotalTimeMillis());
+    }
+}
